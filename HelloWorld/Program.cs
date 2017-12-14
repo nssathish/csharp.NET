69fc3c75cf5cbf4b142e05d9a4c612143f206682
Sathishkumar Subramanian <ssathishkumar@athenahealth.com>
2017-12-14 12:45:07 +0530

    Learning primitive data types

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,6 +6,19 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            //            byte a = 4;
+            //            byte b = 4;
+            //            byte c = a + b;
+            int a = 4;
+            int b = 4;
+            int c = a + b;
+
+            float d = 1.2f;
+            float e = 2.4f;
+
+            float f = d + e;
+            Console.WriteLine("f: " + f);
+            Console.WriteLine("c: " + c);
             Console.WriteLine("Hello World!");
         }
     }
