626a8eab675d0358c15732de05f9f144c7e44543
Sathishkumar Subramanian <neosathishgct@gmail.com>
2018-01-26 18:02:54 +0530

    Nhibernate implementation and Student class discovery

--- NHibernateImpl/NHibernateImpl/Program.cs
+++ NHibernateImpl/NHibernateImpl/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
 
 namespace NHibernateImpl
 {
@@ -10,6 +15,35 @@ namespace NHibernateImpl
     {
         static void Main(string[] args)
         {
+            var cfg = new Configuration();
+            String _dataSource ="Data Source = ICOSWK0033\\SQLEXPRESS";
+            String _initialCatalog = "Initial Catalog = NHibernateImplDB";
+            String _integratedSecurity = "Integrated Security = true";
+            String _timeout = "Connect Timeout = 15";
+            String _encrypt = "Encrypt = False";
+            String _certificate = "TrustServerCertificate = False";
+            String _applicationIntent = "ApplicationIntent = ReadWrite";
+            String _subnetFailover = "MultiSubnetFailover = False";
+
+            cfg.DataBaseIntegration(x =>
+            {
+                //x.ConnectionString = "Data Source + Initial Catalog + Integrated Security + Connect Timeout + Encrypt + TrustServerCertificate + ApplicationIntent + MultiSubnetFailover";
+                x.ConnectionString = _dataSource + _initialCatalog + _integratedSecurity + _timeout + _encrypt + _certificate + _applicationIntent + _subnetFailover; 
+                x.Driver<SqlClientDriver>();
+                x.Dialect<MsSql2012Dialect>();
+            });
+
+            cfg.AddAssembly(Assembly.GetExecutingAssembly());
+            var sefact = cfg.BuildSessionFactory();
+            using (var session = sefact.OpenSession())
+            {
+                using (var tx = session.BeginTransaction())
+                {
+                    //perform database logic
+                    tx.Commit();
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ NHibernateImpl/NHibernateImpl/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernateImpl
+{
+    class Student
+    {
+        public virtual int ID { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string FirstMidName { get; set; }
+
+        //connection string: 
+        //Data Source=ICOSWK0033\SQLEXPRESS;Initial Catalog=NHibernateImplDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
+
+
+    }
+
+}
